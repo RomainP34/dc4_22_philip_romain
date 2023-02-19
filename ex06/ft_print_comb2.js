c12870d31e786e924f3e2a9ca92c259c7016f1ed
RomainP34 <124811816+RomainP34@users.noreply.github.com>
2023-02-19 19:30:55 +0100

    Add files via upload

---
+++ ex06/ft_print_comb2.js
@@ -0,0 +1,10 @@
+function ft_print_comb2() {
+    for (let i = 0; i <= 8; i++) {
+      for (let j = i + 1; j <= 9; j++) {
+        console.log(`${i} ${j}`);
+      }
+    }
+  }
+
+  ft_print_comb2();
+  
